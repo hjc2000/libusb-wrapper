f0b1a543f94fce6abd7d250f240b9b1521fcae79
hjc2000 <hjc.666@foxmail.com>
2025-09-22 10:45:22 +0800

    修复CMake生成规则冲突

--- project.cmake
+++ project.cmake
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-add_library("${ProjectName}")
-=======
 # 定义生成规则
 add_library("${ProjectName}" STATIC)
->>>>>>> 0db458b5efe86590463eec519cbdac05ef0f50c8
 target_import_src(${ProjectName})
 target_import_libusb(${ProjectName} PUBLIC)
 target_import_base(${ProjectName} PUBLIC)
